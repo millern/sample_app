6a6f39c05e27950d0f3c77c4dafdc7b690afcf2d
= <njmiller6@gmail.com>
2013-02-11 15:56:28 -0800

    Chapter 7 exercises complete

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,13 +11,13 @@ describe "User pages" do
     it { should have_selector('title', text: full_title('Sign up')) }
   end
 
-describe "profile page" do
-	let(:user) {FactoryGirl.create(:user)}
-	before {visit user_path(user)}
+	describe "profile page" do
+		let(:user) {FactoryGirl.create(:user)}
+		before {visit user_path(user)}
 
-	it {should have_selector('h1', text: user.name)}
-	it {should have_selector('title',text:user.name)}
-end
+		it {should have_selector('h1', text: user.name)}
+		it {should have_selector('title',text:user.name)}
+	end
 
 
 	describe "signup" do
@@ -28,6 +28,23 @@ end
 			it "should not create a user" do
 				expect {click_button submit}.not_to change(User,:count)
 			end
+
+			describe "after submission" do
+				before {click_button submit}
+				it {should have_selector('title', text: 'Sign up')}
+				it {should have_content('error')}
+			end
+			describe "email address invalid" do
+				before do
+					fill_in "Email", with:"bad.bad.bad"
+					fill_in "Password", with:"aaa"
+					click_button submit
+				end
+				it {should have_selector('li',text:'Email is invalid')}
+				it {should have_selector('li',text: 'Password is too short')}
+			end
+
+
 		end
 		
 		describe "with valid information" do
@@ -41,7 +58,18 @@ end
 			it "should create a user" do
 				expect {click_button submit}.to change(User,:count).by(1)
 			end
+
+			describe "after saving the user" do
+       	 		before { click_button submit }
+        		let(:user) { User.find_by_email('user@example.com') }
+
+        		it { should have_selector('title', text: user.name) }
+        		it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      		end
+      
 		end
+
+
 
 
 
